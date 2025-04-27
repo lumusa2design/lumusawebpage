afdc2ea46064a2945f5fdb7a7bf6ff0cfe2a37a9
lumusa2design <lumusa2design@gmail.com>
2025-04-27 19:49:59 +0100

    Using local storage

--- lumusaweb/src/app/services/dragonball.service.ts
+++ lumusaweb/src/app/services/dragonball.service.ts
@@ -1,5 +1,12 @@
-import {Injectable, signal} from '@angular/core';
+import {effect, Injectable, signal} from '@angular/core';
 import {Characters} from '../inferfaces/character.interface';
+
+const loadFromLocalStorage = (): Characters[] => {
+  const characters = localStorage.getItem('characters');
+  return characters ? JSON.parse(characters) : [];
+};
+
+
 @Injectable({providedIn: 'root'})
 
 export class DragonballService
@@ -9,10 +16,12 @@ export class DragonballService
     {id: 2, name: "Vegeta", power:8002},
     {id: 3, name: "Piccolo", power:1000},
   ]);
+  saveToLocalStorage = effect(()=> {localStorage.setItem('characters', JSON.stringify(this.characters()))})
+
   AddCharacter(character: Characters)
   {
     this.characters.update((list) => [...list, character]);
+
   }
-  constructor() {
-  }
+
 }
