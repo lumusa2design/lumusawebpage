d5cc3767f0aee77c262fd43fa20c659f2cf40cd7
lumusa2design <lumusa2design@gmail.com>
2025-07-26 01:14:35 +0100

    feat: router link added

--- lumusaweb/src/app/app.routes.ts
+++ lumusaweb/src/app/app.routes.ts
@@ -1,9 +1,20 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from './pages/home/home.component';
+import {AboutmeComponent} from './pages/aboutme/aboutme.component';
+import {LearnComponent} from './pages/learn/learn.component';
+import {ContactComponent} from './pages/contact/contact.component';
 
 export const routes: Routes = [
   {
     path:'', component: HomeComponent,
+  },
+  {
+    path:'aboutme', component:AboutmeComponent,
+  },
+  {
+    path:'learn', component:LearnComponent,
+  }, {
+    path:'contact', component:ContactComponent,
   }
 
 ];
